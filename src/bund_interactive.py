c4869c0e10896ca60e7174d6a0d328e26c8cc67a
Vladimir Ulogov <33033499+vulogov-placeiq@users.noreply.github.com>
2018-01-08 15:31:53 -0500

    Add stub for bund_interactive

---
+++ src/bund_interactive.py
@@ -0,0 +1,7 @@
+class BUND_INTERACTIVE:
+    def __init__(self):
+        pass
+    def preflight(self):
+        print "Hello", self.args
+    def SHELL(self):
+        print self.args
--- src/bund_shell.py
+++ src/bund_shell.py
@@ -3,8 +3,10 @@ from bund_terminal import BUND_TERMINAL
 from bund_cmd import BUND_GEN, BUND_HELP
 from bund_env import BUND_ENV
 from bund_args import BUND_ARGS
+from bund_interactive import BUND_INTERACTIVE
 
-class BUND_SHELL(BUND_TERMINAL, BUND_GEN, BUND_HELP, BUND_ENV, BUND_ARGS):
+class BUND_SHELL(BUND_TERMINAL, BUND_GEN, BUND_HELP,
+                 BUND_ENV, BUND_ARGS, BUND_INTERACTIVE):
     def __init__(self):
         self.doc = []
         BUND_HELP.__init__(self)
@@ -12,5 +14,6 @@ class BUND_SHELL(BUND_TERMINAL, BUND_GEN, BUND_HELP, BUND_ENV, BUND_ARGS):
         BUND_GEN.__init__(self,  "(bund) ver %s" % BUND_VERSION, "bund - BUND Shell")
         BUND_ARGS.__init__(self)
         BUND_TERMINAL.__init__(self)
+        BUND_INTERACTIVE.__init__(self)
     def main_preflight(self):
         self._main_preflight()
